92cce296409a2a1c517dec3972022b3fb523d4a5
batonchiccc <106777553+batonchiccc@users.noreply.github.com>
2026-02-07 02:27:27 +0300

    rotation matching ground(untested)

---
+++ Assets/Scripts/GrindingScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GrindingScript : MonoBehaviour
+    {
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Interactor/PlayerRotationScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Interactor
+{
+    public class PlayerRotationScript : MonoBehaviour
+    {
+        [SerializeField] private LayerMask groundMask;
+        
+        [SerializeField] private AnimationCurve animationCurve;
+
+        private void FixedUpdate()
+        {
+            SurfaceAllignment();
+        }
+
+        private void SurfaceAllignment()
+        {
+            Ray ray = new Ray(transform.position, -transform.up);
+            RaycastHit info = new RaycastHit();
+            Quaternion RotationRef = Quaternion.Euler(0f, 0f, 0f);
+
+            if(Physics.Raycast(ray, out info, groundMask))
+            {
+                RotationRef = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector3.up, info.normal), 1f); //, animationCurve.Evaluate(Time)
+                transform.rotation = Quaternion.Euler(RotationRef.eulerAngles.x, RotationRef.eulerAngles.y, RotationRef.eulerAngles.z);
+            }
+        }
+    }
+}
+
+
+
+//upright = Vector3.Cross(transform.right, -(fHit.point - bHit.point).normalized)
+
+//transform.rotation = Quaternion.LookRotation(Vector3.Cross(transform.right, upright));
